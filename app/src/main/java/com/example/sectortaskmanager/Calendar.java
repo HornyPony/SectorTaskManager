271de00291ca8654d0fdc7c368f65e6d571c40b4
Anvar Vagapov <anvar.vagapov14@list.ru>
2020-09-04 19:09:04 +0300

    Можно сделать напоминание

---
+++ app/src/main/java/com/example/sectortaskmanager/Calendar.java
@@ -0,0 +1,14 @@
+package com.example.sectortaskmanager;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.CalendarContract;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Calendar {
+
+
+
+}
--- app/src/main/java/com/example/sectortaskmanager/MainActivity.java
+++ app/src/main/java/com/example/sectortaskmanager/MainActivity.java
@@ -3,14 +3,35 @@ package com.example.sectortaskmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-private ViewPager2 viewPager2;
+    private ImageButton calendarImageButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        calendarImageButton = findViewById(R.id.calendarImageButton);
+        View.OnClickListener onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.calendarImageButton:
+                        Intent intent =
+                                new Intent(Intent.ACTION_INSERT, CalendarContract.Events.CONTENT_URI);
+                        startActivity(intent);
+                }
+
+            }
+
+        };
+
+        calendarImageButton.setOnClickListener(onClickListener);
 
     }
 }

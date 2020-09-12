a661bec142567c5b94649fcc7d726065bd5c90f1
Anvar Vagapov <anvar.vagapov14@list.ru>
2020-09-13 01:59:31 +0300

    Сделан главный экран

---
+++ app/src/main/java/com/example/sectortaskmanager/AddEventActivity.java
@@ -0,0 +1,19 @@
+package com.example.sectortaskmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class AddEventActivity extends AppCompatActivity {
+FloatingActionButton addEventButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+
+
+    }
+}

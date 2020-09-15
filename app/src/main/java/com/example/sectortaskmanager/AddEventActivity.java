1cec5bbb43518125735fd4a34d12de52621e8443
Anvar Vagapov <anvar.vagapov14@list.ru>
2020-09-15 19:55:32 +0300

    Сделан экран выбора режима будильника

--- app/src/main/java/com/example/sectortaskmanager/AddEventActivity.java
+++ app/src/main/java/com/example/sectortaskmanager/AddEventActivity.java
@@ -2,18 +2,30 @@ package com.example.sectortaskmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddEventActivity extends AppCompatActivity {
 FloatingActionButton addEventButton;
+Button chooseRingtoneButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
-
+        chooseRingtoneButton = findViewById(R.id.chooseRingtoneButton);
+        chooseRingtoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddEventActivity.this, ChooseNotificationBehaviourActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
+
+
 }
